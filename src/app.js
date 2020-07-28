1f427a021b7b4cc2e9aa8c9bc596d0ed1775ddc8
saramsanches <67020773+saramsanches@users.noreply.github.com>
2020-07-28 19:28:42 +0100

    Show temperature in first page

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "540573f8fa49b4473d94a3c057290a63";
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
